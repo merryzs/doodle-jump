6d92500701a825de25fe8b48bb47662d5f13e6f9
avaesken <avaesken@gaming.tech>
2026-03-11 17:03:20 +0100

    tryout update

--- doodle jump/Platform.h
+++ doodle jump/Platform.h
@@ -1,25 +1,16 @@
 #pragma once
-#include <SFML/Graphics.hpp>
-#include "PlatformType.h"
+#include "lib.h"
 
 class Platform
 {
 public:
-    Platform(sf::Vector2f position, sf::Vector2f size, PlatformType type);
-    
-    void draw(sf::RenderWindow& window) const;
-	void update(float deltatime);
+    Platform(sf::Vector2f pos, sf::Vector2f size);
 
+    void draw(sf::RenderWindow& window) const;
     sf::Vector2f getPosition() const;
     void move(const sf::Vector2f& offset);
-    void breakPlatform();
-    
+
 private:
-    float bounceTimer = 0.f;
-	float speed = 100.f;
-	int direction = 1;
-    float fallSpeed = 0.f;
-	PlatformType type;
     sf::RectangleShape shape;
-    bool isBroken = false;
+    sf::Texture platformTexture;
 };
---
+++ doodle jump/lib.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+
+
+#include <iostream>
+#include <optional>
+#include <string>
+
+
+#include <functional>
+
+
+#include <vector>
+#include <cmath>
